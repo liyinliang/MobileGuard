a248eed0f3c7d4572a5411fca38db2211b2ae934
tan07160735 <838952100@qq.com>
2017-11-12 21:22:48 +0800

    11.8添加黑名单

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m3communicationguard/db/dao/BlackNumberDao.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m3communicationguard/db/dao/BlackNumberDao.java
@@ -34,6 +34,8 @@ public class BlackNumberDao {
         values.put("number",blackContactInfo.phoneNumber);
         values.put("name",blackContactInfo.contactName);
         values.put("mode",blackContactInfo.mode);
+        values.put("type",blackContactInfo.blackType);
+
         long rowid=db.insert("blacknumber",null,values);
         if (rowid==-1){
             return false;
@@ -54,16 +56,14 @@ public class BlackNumberDao {
 
     public List<BlackContactInfo> getPageBlackNumber(int pagenumber,int pagesize){
         SQLiteDatabase db=blackNumberOpenHelper.getReadableDatabase();
-        Cursor cursor=db.rawQuery(
-                "select number,mode,name from blacknumber limit ? offset ?",
-                new String[]{String.valueOf(pagesize),
-                        String.valueOf(pagesize*pagenumber)});
+        Cursor cursor = db.rawQuery("select number,mode,name,type from blacknumber limit ? offset ?",new String[] {String.valueOf(pagesize),String.valueOf(pagesize*pagenumber)});
         List<BlackContactInfo> mBlackContactInfos=new ArrayList<BlackContactInfo>();
         while (cursor.moveToNext()){
             BlackContactInfo info=new BlackContactInfo();
             info.phoneNumber=cursor.getString(0);
             info.mode=cursor.getInt(1);
             info.contactName=cursor.getString(2);
+            info.blackType = cursor.getString(3);
             mBlackContactInfos.add(info);
         }
         cursor.close();
