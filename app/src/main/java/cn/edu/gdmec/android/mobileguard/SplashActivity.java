ad5ec2b54e2a19f88b7d39f6f8609ce6437a3a87
tan07160735 <838952100@qq.com>
2017-11-19 21:51:11 +0800

    11.19杀毒

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/SplashActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/SplashActivity.java
@@ -1,32 +1,42 @@
 package cn.edu.gdmec.android.mobileguard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import cn.edu.gdmec.android.mobileguard.m1home.HomeActivity;
 import cn.edu.gdmec.android.mobileguard.m1home.utils.MyUtils;
 import cn.edu.gdmec.android.mobileguard.m1home.utils.VersionUpdateUtils;
 
 public class SplashActivity extends AppCompatActivity {
     private TextView mTvVersion;
     private String mVersion;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        mVersion= MyUtils.getVersion(getApplicationContext());
-        mTvVersion=(TextView) findViewById(R.id.tv_splash_version);
-        mTvVersion.setText("版本号："+mVersion);
-        final VersionUpdateUtils versionUpdateUtils=new VersionUpdateUtils(mVersion,SplashActivity.this);
-        /*new Thread(){
+        mVersion = MyUtils.getVersion(getApplicationContext());
+        mTvVersion = (TextView) findViewById(R.id.tv_splash_version);
+        mTvVersion.setText("版本号:"+mVersion);
+        //新改动
+        /*VersionUpdateUtils.DownloadCallback downloadCallback = new VersionUpdateUtils.DownloadCallback() {
             @Override
-            public void run(){
-                super.run();
-                versionUpdateUtils.getCloudVersion();
+            public void afterDownload(String filename) {
+                MyUtils.installApk(SplashActivity.this,"Mobileguard.apk");
             }
-        }.start();*/
-
-
+        };
+        final VersionUpdateUtils versionUpdateUtils = new VersionUpdateUtils(mVersion,SplashActivity.this,downloadCallback,HomeActivity.class);
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                versionUpdateUtils.getCloudVersion("http://android2017.duapp.com/updateinfo.html");
+            }
+        }.start();
+        */
+        startActivity(new Intent(this, HomeActivity.class));
+        finish();
     }
+
 }
--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/utils/MyUtils.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/utils/MyUtils.java
@@ -1,22 +1,35 @@
 package cn.edu.gdmec.android.mobileguard.m1home.utils;
 
+
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Environment;
 
-/**
- * Created by LYL on 2017/9/27.
- */
+import java.io.File;
+
 
 public class MyUtils {
     public static String getVersion(Context context){
-        PackageManager packageManager=context.getPackageManager();
+        PackageManager packageManager = context.getPackageManager();
         try {
-            PackageInfo packageInfo=packageManager.getPackageInfo(context.getPackageName(),0);
+            PackageInfo packageInfo = packageManager.getPackageInfo(context.getPackageName(), 0);
             return packageInfo.versionName;
-        } catch (PackageManager.NameNotFoundException e) {
+        }catch (PackageManager.NameNotFoundException e){
             e.printStackTrace();
             return "";
         }
+
     }
+    public static void installApk(Activity activity, String apkFile){
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
+        intent.setDataAndType(Uri.fromFile(
+                new File(Environment.getExternalStoragePublicDirectory("/download/").getPath()+"/"+apkFile)),"application/vnd.android.package-archive");
+        activity.startActivityForResult(intent,0);
+    }
+
 }
